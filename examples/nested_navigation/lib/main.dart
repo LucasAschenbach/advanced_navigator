ec13a97e39481caebe3e85c8675dd28aa2a31f93
Lucas Aschenbach <lucas.aschenbach@gmail.com>
2021-02-28 19:57:29 +0100

    Update main.dart

--- examples/nested_navigation/lib/main.dart
+++ examples/nested_navigation/lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 }
 
 class App extends StatelessWidget {
-  const App({Key key}) : super(key: key);
+  const App({ Key key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
