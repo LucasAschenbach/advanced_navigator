41da41e51e2895de651ee4c78d3634a3f64d36fd
Lucas Aschenbach <lucas.aschenbach@gmail.com>
2021-02-28 20:30:28 +0100

    Add MaterialApp to examples

--- examples/basic/lib/views/app.dart
+++ examples/basic/lib/views/app.dart
@@ -12,30 +12,20 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MediaQuery(
-      data: MediaQueryData.fromWindow(WidgetsBinding.instance.window),
-      child: Directionality(
-        textDirection: TextDirection.ltr,
-        child: Localizations(
-          locale: WidgetsBinding.instance.window.locale,
-          delegates: [DefaultWidgetsLocalizations.delegate, DefaultMaterialLocalizations.delegate],
-          child: Material(
-            child: AdvancedNavigator(
-              paths: {
-                '/': (_) => [
-                  CupertinoPage(key: ValueKey('home'), child: ViewHome()),
-                ],
-                '/items': (_) => [
-                  CupertinoPage(key: ValueKey('home'), child: ViewHome()),
-                ],
-                '/items/{itemId}': (args) => [
-                  CupertinoPage(key: ValueKey('home'), child: ViewHome()),
-                  CupertinoPage(key: ValueKey('item${args['itemId']}'), child: ViewItem(int.parse(args['itemId']))),
-                ],
-              },
-            ),
-          ),
-        ),
+    return MaterialApp(
+      builder: (context, _) => AdvancedNavigator(
+        paths: {
+          '/': (_) => [
+            CupertinoPage(key: ValueKey('home'), child: ViewHome()),
+          ],
+          '/items': (_) => [
+            CupertinoPage(key: ValueKey('home'), child: ViewHome()),
+          ],
+          '/items/{itemId}': (args) => [
+            CupertinoPage(key: ValueKey('home'), child: ViewHome()),
+            CupertinoPage(key: ValueKey('item${args['itemId']}'), child: ViewItem(int.parse(args['itemId']))),
+          ],
+        },
       ),
     );
   }
